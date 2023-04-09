9ea9850c531601983415f082c1fc625340de4b98
Mbogz <jeff.mwaura@student.moringaschool.com>
2023-04-09 13:48:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var customerName = "John";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+        console.log(customerName);
+}
+upperCaseCustomerName();
+
+function setBestCustomer() {
+    bestCustomer = "not Bob";
+    console.log(bestCustomer);
+}
+setBestCustomer();
+
+function overwriteBestCustomer() {
+    bestCustomer = "not Sally";
+    console.log(bestCustomer);
+}
+overwriteBestCustomer();
+
+const leastFavoriteCustomer = "Dave";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Tom";
+    console.log(leastFavoriteCustomer);
+}
+changeLeastFavoriteCustomer();
+
